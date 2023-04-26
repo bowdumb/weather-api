6467b9e50c7c8a4d48d88468fa88a8d6dcbed5af
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-26 00:24:30 -0400

    Small adjustments to HTML/CSS. Still working out JS. EOD 4/25

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var apiKey = "df5e9990ffbf1afeef20c40f5637942e";
+var apiKey = "df5e9990ffbf1afeef20c40f5637942e"; //A variable that stores my personal OpenWeatherMaps API key.
 var atlLat = 33.7488
 var atlLong = -84.3877
+var citySearch = []; //creates a variable with an empty array for storing the content of the getCity function.
+var citySearchText = document.querySelector("#city-search");
 
-
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + atlLat + '&lon=' + atlLong + '&appid=df5e9990ffbf1afeef20c40f5637942e&units')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + atlLat + '&lon=' + atlLong + '&appid=df5e9990ffbf1afeef20c40f5637942e&units')
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     })
 
 
 
@@ -26,6 +27,22 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + atlLat + '&lon='
     //lattitude
     //longitude
 
-    // function(city) {
 
-    // }
+
+    // citySearchText.addEventListener("submit", getCity)
+
+    // var formEl = ""
+    //     if (formEl.addEventListener) {
+    //         formEl.addEventListener("submit", getCity);
+    //         console.log(formEl);
+    //     }
+    
+//   document.getElementById("city-search").subm = function() {getCity()};
+
+
+    
+
+    var citySearchText = document.getElementById("city-search").value;
+    
+    console.log(citySearchText);
+  
