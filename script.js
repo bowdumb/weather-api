b6e7f1ac677a8bc2f271f11f20c9bc5cbe38d07c
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-21 17:30:18 -0400

    Got the fetch call to return the intended city, and a card to be generated containing the necessary data points.

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
 var apiKey = "df5e9990ffbf1afeef20c40f5637942e"; //A variable that stores my personal OpenWeatherMaps API key.
-var atlLat = 33.7488
-var atlLong = -84.3877
 var citySearch = []; //creates a variable with an empty array for storing the content of the getCity function.
 var citySearchText = document.querySelector("#city-search");
+var submitButton = document.querySelector("#submit-button");
+var currentWeatherContainer = document.querySelector('#currentWeather')
+var searchedCities = document.getElementById("saved-cities");
+// var currentWeatherSearchContainer = document.querySelector("#currentWeatherSearch");
 
-// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + atlLat + '&lon=' + atlLong + '&appid=df5e9990ffbf1afeef20c40f5637942e&units')
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-//     })
+// submitButton.addEventListener("click", cityInputSubmit)
 
+var cityInputSubmit = function (event) {
+    event.preventDefault();
+    var cityInput = citySearchText.value.toLowerCase();
+    console.log(cityInput);
+    if (cityInput.length == 0) {
+        return;
+    };
 
+var dateToday = new Date();
+var formatDate = dateToday.toLocaleDateString('en-US', {month: 'numeric', day: 'numeric', year: 'numeric'});
 
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=New York,us-ny,840&limit=1&appid=df5e9990ffbf1afeef20c40f5637942e&units')
-    .then(function(response) {
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + apiKey + '&units=imperial')
+    .then(function (response) {
+        if (response.status !== 200) {
+            console.log(response.status);
+            alert('That is not a valid city. Please enter another city name.');
+            return;
+        }
         return response.json();
     })
-    .then(function(data) {
-        console.log(data);
-    })
+    .then(function (data) {
+        console.log(data)
+        console.log("City Name:", data.city.name);
+        console.log(data.list[0].main.temp);
+        console.log("The current date is: ", dateToday);
+        console.log(data.list[0].wind.speed, "MPH");
+        console.log(data.list[0].main.humidity);
+      
+        var card = document.createElement("div");
+        card.classList.add("card");
 
-    //city name
-    //country
-    //lattitude
-    //longitude
+    var currentCardWeather = `
+    <h2>${data.city.name} (${formatDate})</h2>
+    <p> Current temperature: ${data.list[0].main.temp} °F</p>
+    <p>Wind speed: ${data.list[0].wind.speed} mph</p>
+    <p>Humidity: ${data.list[0].main.humidity}%</p>
+    `
+        card.innerHTML = currentCardWeather;
+        currentWeatherContainer.appendChild(card);
+    });
+};
+
+function cityHistory() {
+    localStorage.setItem('City', cityInput);
+    var citySearches = localStorage.getItem('City');
+    var storedCity = document.createElement("button");
+    storedCity.textContent = citySearches;
+    searchedCities.appendChild(storedCity);
+    console.log(citySearches);
+}
+
+submitButton.addEventListener("click", cityInputSubmit);
 
 
-
-    // citySearchText.addEventListener("submit", getCity)
-
-    // var formEl = ""
-    //     if (formEl.addEventListener) {
-    //         formEl.addEventListener("submit", getCity);
-    //         console.log(formEl);
-    //     }
-    
-//   document.getElementById("city-search").subm = function() {getCity()};
-
-
-    
-
-    var citySearchText = document.getElementById("city-search").value;
-    
-    console.log(citySearchText);
-  
